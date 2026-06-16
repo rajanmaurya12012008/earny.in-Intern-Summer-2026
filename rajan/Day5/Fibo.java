5377dd1de87a7ef2984725a70a38ea57fd9558b2
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-16 17:56:30 +0530

    Day 5 Fibonacci Serie Sloution

---
+++ rajan/Day5/Fibo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many Fibonacci numbers to print: ");
+        int n = sc.nextInt();
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        sc.close();
+    }
+}
