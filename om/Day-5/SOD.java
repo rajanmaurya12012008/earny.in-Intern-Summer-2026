b1acaab29ed8086440ca70e21ed97544d6ed7999
connectmeom-droid <connectme.rajom@gmail.com>
2026-06-18 17:26:19 +0530

    All day5 que done

---
+++ om/Day-5/SOD.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SOD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int sum = 0;
+
+        while (number != 0) {
+
+            int digit = number % 10;
+            sum = sum + digit;
+
+            number = number / 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
+    }
+}
