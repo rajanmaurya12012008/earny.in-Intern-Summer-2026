ea1845161866f8fb22588fa79cf138b146f2ffba
sanjana. <sanjanakum552@gmail.com>
2026-06-18 18:11:36 +0530

    Added sum.py

---
+++ sanjana/Day 5/sum.py
@@ -0,0 +1,11 @@
+# Take input from user
+num = int(input("Enter a number: "))
+
+sum_digits = 0
+
+while num > 0:
+    digit = num % 10
+    sum_digits += digit
+    num = num // 10
+
+print("Sum of digits =", sum_digits)
