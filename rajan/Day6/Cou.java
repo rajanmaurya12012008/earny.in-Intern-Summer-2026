a8e974057b7ee1c9d7ea00df997d7eb2a30ef6f0
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-21 14:02:32 +0530

    Display course using Define function

---
+++ rajan/Day6/Cou.java
@@ -0,0 +1,12 @@
+public class Cou {
+    public static void course_detail() {
+        System.out.println("Course Name: Python Programming");
+        System.out.println("Course Duration: 3 Months");
+        System.out.println("Instructor: Rajan Maurya");
+        System.out.println("Difficulty level: Beginner");
+    }
+
+    public static void main(String[] args) {
+        course_detail();
+    }
+}
