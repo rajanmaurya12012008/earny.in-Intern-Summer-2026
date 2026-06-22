99a1573f2140345adaaf8c3ba0be1ffdaa076327
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-22 11:58:47 +0530

    welcome message

---
+++ rajan/Day6/Wel.java
@@ -0,0 +1,14 @@
+public class Wel {
+
+    public static void welcome_message() {
+        System.out.println("=================================");
+        System.out.println("Welcome To The Our Internship Program");
+        System.out.println("Learn . Build . Grow");
+        System.out.println("=================================");
+    }
+
+    public static void main(String[] args) {
+        welcome_message();
+    }
+}
+
